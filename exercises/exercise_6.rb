bd3a107f1477846f6c3629de88dd47d195736179
Amy Bartels <akunrau@gmail.com>
2020-05-26 03:40:22 +0000

    update Employee and Store classes. Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Unrau", last_name: "Sarah", hourly_rate: 50)
+@store1.employees.create(first_name: "Bartels", last_name: "Nolan", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Johnston", last_name: "Tim", hourly_rate: 60)
+@store2.employees.create(first_name: "Fast", last_name: "Mackenzie", hourly_rate: 90)
+@store2.employees.create(first_name: "Walde", last_name: "Tannis", hourly_rate: 40)
+@store2.employees.create(first_name: "McDonnell", last_name: "Gemma", hourly_rate: 80)
+
+allEmployees = Employee.all
+puts allEmployees.count
